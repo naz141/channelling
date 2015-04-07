df8318ae1c87a1df6fb8a2b906ab9ab1b9d8cddd
naz141 <anm.nasrullah@gmail.com>
2015-04-07 10:30:41 +0300

    with working apk

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\phonegap\hello\platforms\android\bin\AndroidManifest.xml : \
+C:\phonegap\hello\platforms\android\AndroidManifest.xml \
---
+++ platforms/android/bin/MainActivity-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+C:\phonegap\hello\platforms\android\bin\MainActivity-release-unsigned.apk : \
+C:\phonegap\hello\platforms\android\bin\MainActivity.ap_ \
+C:\phonegap\hello\platforms\android\bin\classes.dex \
+C:\phonegap\hello\platforms\android\CordovaLib\ant-build\classes.jar \
